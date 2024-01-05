3fb21d588a1a779e7c276a6dc3acdf36553604a4
Aman <sriaman3@gmail.com>
2024-01-05 16:28:23 +0530

    change in TC

--- src/test/java/com/qa/amz/tests/FirstTest.java
+++ src/test/java/com/qa/amz/tests/FirstTest.java
@@ -1,9 +1,10 @@
 package com.qa.amz.tests;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -19,11 +20,13 @@ public class FirstTest {
 	 */
 	
 	@Test
-	public void doClickOnRegisterButton() throws InterruptedException {
+	public void doClickOnRegisterButton() throws InterruptedException, MalformedURLException {
 		
 		EdgeOptions options = new EdgeOptions();
+		//options.addArguments(Capabilities {browserName: MicrosoftEdge, ms:edgeOptions: {args: [--start-maximized], extensions: []}});
 		options.addArguments("--start-maximized");
-		driver = new EdgeDriver();	//top casting
+		
+		driver = new RemoteWebDriver(new URL("localhost:10.1.0.40/wb/hub"), options);	//top casting
 		
 		driver.get("https://naveenautomationlabs.com/opencart/index.php?route=account/login");
 		
